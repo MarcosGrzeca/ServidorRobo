199cb65debb7901ffe4382ad8c2dba56b9883d43
MarcosGrzeca <marcos-a-g@hotmail.com>
2015-06-06 14:32:17 -0300

    Versão inicial Serviidor

---
+++ src/MyWebSocketHandler.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+
+import jpl.Query;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class MyWebSocketHandler {
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+        System.out.println("Close: statusCode=" + statusCode + ", reason=" + reason);
+    }
+
+    @OnWebSocketError
+    public void onError(Throwable t) {
+        System.out.println("Error: " + t.getMessage());
+    }
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        System.out.println("Connect: " + session.getRemoteAddress().getAddress());
+        try {
+
+            String t2 = "consult('C:/Users/Marcos/Documents/NetBeansProjects/AppJavaProlog/AppJavaProlog/src/cods_prolog/planejador.pl')";
+            Query q2 = new Query(t2);
+            System.out.println( t2 + " " + (q2.hasSolution() ? "correcto" : "fallo") );
+//          
+
+            String t3 = "consult('C:/Users/Marcos/Documents/NetBeansProjects/AppJavaProlog/AppJavaProlog/src/cods_prolog/trabalho2.pl')";
+            Query q3 = new Query(t3);
+            System.out.println( t3 + " " + (q3.hasSolution() ? "correcto" : "fallo") );
+
+            String t4 = "teste14";
+            Query q4 = new Query(t4);
+            //System.out.println( t4 + " " + (q4.hasSolution() ? "correcto" : "fallo") );
+            
+            //String teste = q4.hasSolution();
+            session.getRemote().sendString("Hello Webbrowser EU SOU O CARA");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String message) {
+        System.out.println("Message: " + message);
+    }
+}
